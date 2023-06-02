35096e54404bb2a520cce7564662e1e10e321c28
skenrobert <skenrobert@gmail.com>
2023-06-02 14:47:25 -0500

    update

---
+++ tasks/ulr.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
+from tasks import views
+
+router = routers.DefaultRouter()
+router.register(r"tasks", views.TaskView, "tasks")
+
+#api versioning
+urlpatterns = [
+    path("api/v1/", include(router.urls)), #GET, POST, PUT, DELETE
+    path('docs/', include_docs_urls(title='Tasks API')),
+]
