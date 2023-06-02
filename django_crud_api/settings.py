d5fbaadb292225cef609ba39c27a40c3ca8b7e0c
skenrobert <skenrobert@gmail.com>
2023-06-02 15:57:58 -0500

    update

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'coreapi',
     'tasks',
-
 
 ]
 
@@ -129,6 +129,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
-]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
--- tasks/urls.py
+++ tasks/urls.py
@@ -11,4 +11,5 @@ router.register(r"tasks", views.TaskView, "tasks")
 #api versioning (v1 example)
 urlpatterns = [
     path("api/v1/", include(router.urls)), #GET, POST, PUT, DELETE
+    path('docs/', include_docs_urls(title='Tasks API')),
 ]
