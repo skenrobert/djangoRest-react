41aa54bbc139e2d350938ede4b7d72d056797027
skenrobert <skenrobert@gmail.com>
2023-06-02 14:58:59 -0500

    update

--- tasks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Task(models.Model): #model for migration
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    done = models.BooleanField(default=False)
-
-    def __str__(self): # show values in http://127.0.0.1:8000/admin/tasks
-        return self.title # before show obj no title
--- tasks/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .serializer import TaskSerializer
-from .models import Task
-
-# Create your views here.
-class TaskView(viewsets.ModelViewSet):
-    serializer_class = TaskSerializer
-    queryset = Task.objects.all()
