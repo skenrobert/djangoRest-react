7166ab4865b4203e8444ba43b8d742d5a253d1a0
skenrobert <skenrobert@gmail.com>
2023-06-02 12:48:45 -0500

    update change

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model): #model for migration
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self): # show values in http://127.0.0.1:8000/admin/tasks
+        return self.title
